938946c7726745d56c7210734054601d1bbeba59
isakulovDev <81306984+isakulovDev@users.noreply.github.com>
2021-06-13 10:51:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+TELEGRAM_BOT_TOKEN = 'Your API Token'
+bot = telebot.TeleBot("TELEGRAM_BOT_TOKEN")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    print(message)
+    bot.send_message(message.chat.id, "Assalomu alaykum , bot guruhingizdagi kirdi-chiqdilarni tozalaydi.")
+@bot.message_handler(content_types=['new_chat_members'])
+def echo_all(message):
+	bot.delete_message(message.chat.id, message.id)
+@bot.message_handler(content_types=['left_chat_member'])
+def delete(message):
+    bot.delete_message(message.chat.id, message.id)
+
+bot.polling()
