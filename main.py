15e338a8ffe98bde8c6746efd5f9608a8ad8f294
isakulovDev <81306984+isakulovDev@users.noreply.github.com>
2021-06-13 10:57:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import telebot
-TELEGRAM_BOT_TOKEN = 'Your API Token'
-bot = telebot.TeleBot("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = 'Your TOKEN'
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    print(message)
     bot.send_message(message.chat.id, "Assalomu alaykum , bot guruhingizdagi kirdi-chiqdilarni tozalaydi.")
 @bot.message_handler(content_types=['new_chat_members'])
 def echo_all(message):
