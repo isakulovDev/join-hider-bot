9000dd940b1be3a1e9dc6ceb1c0c99cce98c23df
isakulovDev <81306984+isakulovDev@users.noreply.github.com>
2021-06-30 16:26:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 def send_welcome(message):
     bot.send_message(message.chat.id, "Assalomu alaykum , bot guruhingizdagi kirdi-chiqdilarni tozalaydi.")
 @bot.message_handler(content_types=['new_chat_members'])
-def echo_all(message):
+def delete_msg(message):
 	bot.delete_message(message.chat.id, message.id)
-@bot.message_handler(content_types=['left_chat_member'])
-def delete(message):
-    bot.delete_message(message.chat.id, message.id)
-
 bot.polling()
